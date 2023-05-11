6fa41a2c9a5a585bcf32cb8d1d928622bf7e544b
Natanael Alves <natanalvespr@gmail.com>
2023-05-11 14:52:28 -0300

    iniciando

---
+++ UberSwiftUi/UberSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUiApp.swift
+//  UberSwiftUi
+//
+//  Created by Natanael Alves Pereira on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
